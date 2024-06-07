6c40870ed79b8f9322b8d9957e86eaed5cbb357b
Matt South <matt@ntnt.co>
2024-06-07 09:15:58 -0600

    many misc changes

--- pound.c
+++ pound.c
@@ -1,27 +1,46 @@
+/*** Includes ***/
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+/*** Defines ***/
 
-struct termios orig_termios;
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** Data ***/
+
+struct editor_config {
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+
+struct editor_config E;
+
+/*** Terminal ***/
 
 void die(const char *s) {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
 
 void disable_raw_mode() {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 		die("tcsetattr");
 }
 
 void enable_raw_mode() {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
 	atexit(disable_raw_mode);
 
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag &= ~(CS8);
@@ -32,20 +51,86 @@ void enable_raw_mode() {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-int main() {
-	enable_raw_mode();
+char editor_read_key() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
 
+int get_cursor_position(int *rows, int *cols) {
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
 
-	while (1) {
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+	printf("\r\n");
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1) {
 		if (iscntrl(c)) {
 			printf("%d\r\n", c);
 		} else {
 			printf("%d ('%c')\r\n", c, c);
 		}
-		if (c == 'q') break;
 	}
+	editor_read_key();
+	return -1;
+}
 
+int get_window_size(int *rows, int *cols) {
+	struct winsize ws;
+
+	if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+		editor_read_key();
+		return get_cursor_position(rows, cols);
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+/*** Output ***/
+
+void editor_draws_rows() {
+	int y;
+	for (y = 0; y < E.screenrows; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void editor_refresh_screen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	editor_draws_rows();
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** Input ***/
+
+void editor_process_keypress() {
+	char c = editor_read_key();
+	switch (c) {
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+}
+
+/*** Init ***/
+
+void init_editor() {
+	if (get_window_size(&E.screenrows, &E.screencols) == -1) die("get_window_size");
+}
+
+int main() {
+	enable_raw_mode();
+	init_editor();
+
+	while (1) {
+		editor_refresh_screen();
+		editor_process_keypress();
+	}
 	return 0;
 }
